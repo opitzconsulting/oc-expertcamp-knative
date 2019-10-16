970ee1a1b821368e878ecad853d9c28ac7ba2bae
AFG <AFG@NLW-2331.opitz-consulting.int>
2019-10-16 12:33:07 +0200

    Initial setup

---
+++ helloworld/src/main/java/com/opitzconsulting/expertcamp/helloworld/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package com.opitzconsulting.expertcamp.helloworld.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
