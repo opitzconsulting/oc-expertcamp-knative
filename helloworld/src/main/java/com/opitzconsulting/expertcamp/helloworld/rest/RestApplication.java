f09541bbc3ddb0dd5500f144491228fbe4088cec
AFG <AFG@NLW-2331.opitz-consulting.int>
2019-10-16 14:06:01 +0200

    Removed since not needed

--- helloworld/src/main/java/com/opitzconsulting/expertcamp/helloworld/rest/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.opitzconsulting.expertcamp.helloworld.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class RestApplication extends Application {
-
-}
